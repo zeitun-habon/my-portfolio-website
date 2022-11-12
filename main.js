09146418b0348c1c38e0d69ea9ab9e7d9128c367
zeitun-habon <zeitunhabiba7@gmail.com>
2022-11-12 03:27:33 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".navbar-nav li").click(function(){
+      $(".navbar-nav li").removeClass("active");
+      $(this).addClass("active");
+    }
+    );
+
+    $('.navbar-nav a').click(function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    });
+
+  }
+  );
